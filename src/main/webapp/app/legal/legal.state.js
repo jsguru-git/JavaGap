81d9feeb540c61db4f5f9f8a1795f1e0ee83739d
John Wilson <johnwilson119@outlook.com>
2017-03-01 04:13:06 +0100

    #1. made done Help page an accordion type. #2. made done Legal page as requirements(2-1.the left menu stays on screen as user scrolls up and down the page. 2-2.auto-scroll down to the section on the page when user selects a menu.)

---
+++ src/main/webapp/app/legal/legal.state.js
@@ -0,0 +1,50 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('javagapApp')
+        .config(stateConfig);
+
+    stateConfig.$inject = ['$stateProvider'];
+
+    function stateConfig($stateProvider) {
+        $stateProvider.state('legal', {
+            parent: 'app',
+            url: '/legal/:scrollTo',
+            views: {
+                '@': {
+                    templateUrl: 'app/layouts/content.html'
+                },
+                'content@legal': {
+                    templateUrl: 'app/legal/legal.html',
+                    controller: 'LegalController',
+                    controllerAs: 'vm',
+                    resolve: {
+                        target: ['$stateParams', function($stateParams) {
+                            return $stateParams.scrollTo;
+                        }]
+                    }
+                },
+                'header@legal': {
+                    templateUrl: 'app/layouts/header/header.html',
+                    controller: 'NavbarController',
+                    controllerAs: 'vm'
+                },
+                'footer@legal': {
+                    templateUrl: 'app/layouts/footer/footer.html',
+                    controller: 'NavbarController',
+                    controllerAs: 'vm'
+                }
+            },
+            onEnter: scrollTo
+        });
+
+        scrollTo.$inject = ['$location', '$stateParams', '$anchorScroll', '$timeout'];
+        function scrollTo($location, $stateParams, $anchorScroll, $timeout) {
+            $timeout(function() { 
+                $location.hash($stateParams.scrollTo);
+                $anchorScroll();
+            }, 100);
+        }
+    }
+})();
--- src/main/webapp/app/pages/help/help.controller.js
+++ src/main/webapp/app/pages/help/help.controller.js
@@ -5,10 +5,75 @@
         .module('javagapApp')
         .controller('HelpController', HelpController);
 
-    HelpController.$inject = ['Principal', 'Auth', 'JhiLanguageService', '$translate'];
+    HelpController.$inject = ['Principal', '$scope', '$compile'];
 
-    function HelpController (Principal, Auth, JhiLanguageService, $translate) {
-        
-       
+    function HelpController (Principal, $scope, $compile) {
+      $scope.activeArray = 1;
+	  $scope.coord = '';
+	  $scope.accordionConfig = {
+	    debug: false,
+	    animDur: 300,
+	    expandFirst: false,
+	    autoCollapse: true,
+	    watchInternalChanges: false,
+	    headerClass: '',
+	    beforeHeader: '',
+	    afterHeader: '<div class="drop-icon-wrapper sir-accordion-vertical-align"><i class="glyphicon glyphicon-chevron-down"></i></div>',
+	    topContentClass: '',
+	    beforeTopContent: '',
+	    afterTopContent: '<div><p><small>I repeat through all accordion</small></p></div>',
+	    bottomContentClass: '',
+	    beforeBottomContent: '',
+	    afterBottomContent: ''
+	  };
+
+	  $scope.accordionArray = 
+	  [
+	    {	
+	    	"title":"How do I install the theme?",
+	    	"topContent":"DIY kinfolk etsy, drinking vinegar put a bird on it taxidermy chicharrones humblebrag kombucha "
+	    				+ "wolf beard swag tofu vegan try-hard.Celiac thundercats yr tote bag, "
+	    				+ "pickled small batch lo-fi dreamcatcher occupy chicharrones pop-up chambray letterpress.",
+	    	"bottomContent":"Hammock PBR&B crucifix, green juice vinyl neutra tacos cold-pressed. Next level meggings "
+	    				+ "try-hard chicharrones, sustainable mustache franzen. Flannel yr brooklyn skateboard literally "
+	    				+ "neutra scenester, VHS authentic listicle green juice."
+	    },
+	    {	
+	    	"title":"How can I import the demo content?",
+	    	"topContent":"DIY kinfolk etsy, drinking vinegar put a bird on it taxidermy chicharrones humblebrag kombucha "
+	    				+ "wolf beard swag tofu vegan try-hard.Celiac thundercats yr tote bag, "
+	    				+ "pickled small batch lo-fi dreamcatcher occupy chicharrones pop-up chambray letterpress.",
+	    	"bottomContent":"Hammock PBR&B crucifix, green juice vinyl neutra tacos cold-pressed. Next level meggings "
+	    				+ "try-hard chicharrones, sustainable mustache franzen. Flannel yr brooklyn skateboard literally "
+	    				+ "neutra scenester, VHS authentic listicle green juice."
+	    },
+	    {	
+	    	"title":"Do I need to activate the plugins?",
+	    	"topContent":"DIY kinfolk etsy, drinking vinegar put a bird on it taxidermy chicharrones humblebrag kombucha "
+	    				+ "wolf beard swag tofu vegan try-hard.Celiac thundercats yr tote bag, "
+	    				+ "pickled small batch lo-fi dreamcatcher occupy chicharrones pop-up chambray letterpress.",
+	    	"bottomContent":"Hammock PBR&B crucifix, green juice vinyl neutra tacos cold-pressed. Next level meggings "
+	    				+ "try-hard chicharrones, sustainable mustache franzen. Flannel yr brooklyn skateboard literally "
+	    				+ "neutra scenester, VHS authentic listicle green juice."
+	    },
+	    {	
+	    	"title":"Where can I change the colours?",
+	    	"topContent":"DIY kinfolk etsy, drinking vinegar put a bird on it taxidermy chicharrones humblebrag kombucha "
+	    				+ "wolf beard swag tofu vegan try-hard.Celiac thundercats yr tote bag, "
+	    				+ "pickled small batch lo-fi dreamcatcher occupy chicharrones pop-up chambray letterpress.",
+	    	"bottomContent":"Hammock PBR&B crucifix, green juice vinyl neutra tacos cold-pressed. Next level meggings "
+	    				+ "try-hard chicharrones, sustainable mustache franzen. Flannel yr brooklyn skateboard literally "
+	    				+ "neutra scenester, VHS authentic listicle green juice."
+	    },
+	    {	
+	    	"title":"How can I get support?",
+	    	"topContent":"DIY kinfolk etsy, drinking vinegar put a bird on it taxidermy chicharrones humblebrag kombucha "
+	    				+ "wolf beard swag tofu vegan try-hard.Celiac thundercats yr tote bag, "
+	    				+ "pickled small batch lo-fi dreamcatcher occupy chicharrones pop-up chambray letterpress.",
+	    	"bottomContent":"Hammock PBR&B crucifix, green juice vinyl neutra tacos cold-pressed. Next level meggings "
+	    				+ "try-hard chicharrones, sustainable mustache franzen. Flannel yr brooklyn skateboard literally "
+	    				+ "neutra scenester, VHS authentic listicle green juice."
+	    }
+	  ];
     }
 })();
